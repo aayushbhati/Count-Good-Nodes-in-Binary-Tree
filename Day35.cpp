d9653ce06192c1126325fedfe66b26ffee06055a
aayushbhati <124025774+aayushbhati@users.noreply.github.com>
2025-08-07 22:31:10 +0530

    Day35.cpp

---
+++ Day35.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void solve(TreeNode* root,int &count,int maxi){
+    if(root==NULL)return;
+    if(root->val>=maxi){
+        count++;
+        maxi=root->val;
+    }
+    solve(root->left,count,maxi);
+    solve(root->right,count,maxi);
+}
+    int goodNodes(TreeNode* root) {
+        int maxi=INT_MIN,count=0;
+        solve(root,count,maxi);
+        return count;
+    }
+};
